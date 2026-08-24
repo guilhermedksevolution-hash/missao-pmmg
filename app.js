695de9bacb1b330f5793487f3a51e00667c17c39
guilhermedksevolution-hash <guilhermedksevolution@gmail.com>
2026-08-23 21:06:51 -0300

    Add function to start study mission with alert

---
+++ app.js
@@ -0,0 +1,8 @@
+function comecarEstudo() {
+  alert(
+    "🚔 MISSÃO INICIADA!\\n\\n" +
+    "📖 Matéria: Português\\n" +
+    "🎯 Assunto: Interpretação de texto\\n\\n" +
+    "Em breve esta tela abrirá sua primeira aula completa."
+  );
+}
